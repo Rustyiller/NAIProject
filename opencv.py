f6a9dd3ddb7170e9f80efb96b97b7b02f3e51542
Robert Deyk <36889034+Rustyiller@users.noreply.github.com>
2019-06-01 12:18:40 +0200

    dodano wykrywanie oczu + dodano komentarze

--- opencv.py
+++ opencv.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 
 face_cascade = cv2.CascadeClassifier('casscades/data/haarcascade_frontalface_alt2.xml')
+eye_cascade = cv2.CascadeClassifier('casscades/data/haarcascade_eye.xml')
 #ustawienie śledzenia obrazu z kamery
 cap = cv2.VideoCapture(0)
 
@@ -12,21 +13,22 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.5, 5)
+    faces = face_cascade.detectMultiScale(gray)
 
     for (x,y,w,h) in faces:
        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
        roi_gray = gray[y:y+h, x:x+w]
        roi_color = frame[y:y+h, x:x+w]
 
+       eyes = eye_cascade.detectMultiScale(roi_gray)
+       for (ex, ey, ew, eh) in eyes:
+           cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
     cv2.imshow('frame', frame)
 
-    #ustawianie koloru szarego
 
-
-   # cv2.imshow('gray', gray)
 
     #wyłączenie programu po kliknięciu q
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-
+cap.release()
+cv2.destroyAllWindows()
